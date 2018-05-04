80f493f67d62ea196500fd76df38e1ddf906b357
pchaubal <prakrutchaubal@gmail.com>
2018-05-04 10:38:17 +0530

    Added gaussian generator for points generator

--- visco/sampling_position.py
+++ visco/sampling_position.py
@@ -3,24 +3,37 @@ import os
 
 R = 10.0
 Box_size = 60.0
+binsize = 0.5 # In the units of Mpc
+max_r = 70.0
 
+def generate_pair(distance):
 
-def generate_pair(r):
+	found_pair = False
+	while (found_pair==False):
+		r1 = np.random.rand(3)
+		r2 = np.random.rand(3)
+		d  = np.linalg.norm(r1-r2)
 
-	r1 = np.random.rand(3)
-	r2 = np.random.rand(3)
-	d  = np.linalg.norm(r1-r2)
+		r1 = (distance/d)*r1
+		r2 = (distance/d)*r2
 
+		if (np.max(r1)<Box_size) and (np.max(r2)<Box_size):
+			found_pair = True
+			# print np.linalg.norm(r1-r2)
 
-	r1 = (r/d)*r1
-	r2 = (r/d)*r2
-
-	# print r1,r2
-	# d= np.linalg.norm(r1-r2)
-	print np.linalg.norm(r1-r2)
 	return ([r1,r2])
 
-print generate_pair(10.0)
-# for i in range(len(r1)):
-# 	if r[i]< Box_size:
-# 		rlist.append(r1)
+print generate_pair(7.0)
+
+
+r_list = []
+r_pairs = []
+for bin_no in np.arange(max_r/binsize):
+	print bin_no*binsize, (bin_no+1)*binsize
+	for i in range(1000):
+		r = 0.5*binsize*np.random.randn() + (bin_no*binsize +binsize*0.5)
+		r_pairs.append(generate_pair(r))
+	r_list.append([[bin_no*binsize, (bin_no+1)*binsize],r_pairs])
+	del r_pairs[:]
+
+print len(r_list)
